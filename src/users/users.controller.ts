5a2f41aa1ca6cd81b73e51ee27a39b7bfe49aebc
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-08-09 15:11:31 +0100

    development

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,52 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  Get,
+  Query,
+  Delete,
+  Patch,
+  NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     console.log(body);
     const { email, password } = body;
+
+    this.usersService.create(email, password);
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(Number(id));
+    if (!user) throw new NotFoundException('User not found!');
+    return user;
+  }
+
+  @Get()
+  findAll(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(Number(id), body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(Number(id));
   }
 }
