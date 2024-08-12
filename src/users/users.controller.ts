f69a46eec552771c4e294b0c9426f47a0235746e
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-08-12 12:30:45 +0100

    implemented auth services

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,18 +14,19 @@ import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService, private authService: AuthService) {}
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     console.log(body);
     const { email, password } = body;
 
-    this.usersService.create(email, password);
+    this.authService.signup(email, password);
   }
 
   // @UseInterceptors(new SerializeInterceptor(UserDto))
