947735f52e53c47434d09c20c9b42ea1fd52addd
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-08-13 10:23:19 +0100

    setup relationships with typeorm

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, UseGuards } from '@nestjs/common';
+import { Post } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from 'src/guards/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+  @Post()
+  createReports(@Body() body: CreateReportDto) {
+    return this.reportsService.create(body);
+  }
+}
