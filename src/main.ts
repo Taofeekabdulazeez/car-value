6f66f08121b0c7b1099fccfb92eace696a15451f
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-08-18 14:04:06 +0100

    added cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ const cookieSession = require('cookie-session');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieSession({ keys: ['asdfasfd'] }));
+  app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
